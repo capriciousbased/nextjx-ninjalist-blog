7201519541b4bbd0a0d1019c766a0fee59e96590
capriciousbased <mdanisulhoquekhan@gmail.com>
2021-06-23 07:57:45 +0200

    navbar added

---
+++ pages/about.js
@@ -0,0 +1,10 @@
+const About = () => {
+    return ( 
+        <div>
+            <h1>About</h1>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iure omnis quae consequatur id eveniet sapiente enim ducimus aut. Laborum quos vero rem iure eligendi sit? Nemo fuga corrupti minus rem.</p>
+        </div>
+     );
+}
+ 
+export default About;
--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,14 @@
 import Head from 'next/head'
 import Image from 'next/image'
+import Navbar from '../comps/Navbar'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
   return (
     <div>
-  <h1>Hello World</h1>
+    <Navbar />
+  <h1>HomePage</h1>
+  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus atque possimus aliquid. Omnis, porro odio necessitatibus veniam consequatur earum praesentium quaerat repudiandae est maxime, amet eaque ullam, ad fuga quidem?</p>
     </div>
   )
 }
